2b895b58bcb0e41720c7cf31a7e8e842aa7dda34
Terry Caliendo <terrycaliendo@gmail.com>
2021-10-01 16:25:13 -0700

    Add command in model

--- src/Model/database.ts
+++ src/Model/database.ts
@@ -1,6 +1,12 @@
-export let database = [1, 2, 3, 4];
+import { executionStatus } from "../types/commands";
 
-export function databaseAdd(key: string, value: string): string {
+let database = new Map<string, [string]>();
+
+export function databaseAdd(key: string, value: string): executionStatus {
   console.log("adding to database", key, value);
-  return "complete";
+  let foundKey = database.get(key);
+  if (foundKey) foundKey.push(value);
+  else database.set(key, [value]);
+  console.log("database:", database.entries());
+  return { success: true, continue: true };
 }
