7007a1fb41279fa589c2709cb27b207db29140f2
Terry Caliendo <terry@dedicatedmanagers.com>
2021-10-02 10:04:48 -0700

    Member already exists feature on databaseAdd

--- src/Model/Database.Functions.ts
+++ src/Model/Database.Functions.ts
@@ -2,11 +2,31 @@ import { executionStatus } from "../types/commands";
 
 let database = new Map<string, [string]>();
 
-export function databaseAdd(key: string, value: string): executionStatus {
-  console.log("adding to database", key, value);
-  let foundKey = database.get(key);
-  if (foundKey) foundKey.push(value);
-  else database.set(key, [value]);
+// Adds a Member to the collection for a given key
+// Displays an error if the member already exixts for the key
+export function databaseAdd(
+  collectionName: string,
+  memberName: string
+): executionStatus {
+  console.log("adding to database", collectionName, memberName);
+  let collection = database.get(collectionName);
+  console.log("collection", collection);
+  // If the member already exists, return an error
+  if (
+    collection?.find((member) => {
+      console.log("finding member", member, memberName);
+      return member === memberName;
+    }) != undefined
+  )
+    return {
+      success: false,
+      continue: true,
+      message: "ERROR, member already exists for key.",
+    };
+  // Collection name is found, Add the new member
+  if (collection) collection.push(memberName);
+  // Collection name is not found, add the new collection with the new member in a new array
+  else database.set(collectionName, [memberName]);
   // console.log("database:", database.entries());
   return { success: true, continue: true, message: "Added" };
 }
