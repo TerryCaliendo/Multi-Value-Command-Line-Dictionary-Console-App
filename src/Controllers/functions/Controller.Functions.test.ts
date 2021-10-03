12d5f3b98693ad4cef3867ffa496b31876cd8ea5
Terry Caliendo <terry@dedicatedmanagers.com>
2021-10-02 17:18:50 -0700

    Testing: Keys (controller)

--- src/Controllers/functions/Controller.Functions.test.ts
+++ src/Controllers/functions/Controller.Functions.test.ts
@@ -1,22 +1,51 @@
 import { executeUserCommand } from "./Controller.Functions";
+import { executionCommands } from "../../types/commands";
 
 //////////////////////////////
 // Controller Functions
 //////////////////////////////
 
-// - executeUserCommand()
+// ADD - executeUserCommand()
 describe("Controller Functions - executeUserCommand()", () => {
   // check valid input
   it("ADD foo bar", () => {
     expect(
-      executeUserCommand({ action: "ADD", input1: "foo", input2: "bar" })
+      executeUserCommand({
+        action: executionCommands.add,
+        input1: "foo",
+        input2: "bar",
+      })
     ).toMatchObject({ continue: true, success: true });
   });
   // check invalid input (missing parameter)
   it("ADD bar", () => {
-    expect(executeUserCommand({ action: "ADD", input1: "bar" })).toMatchObject({
+    expect(
+      executeUserCommand({ action: executionCommands.add, input1: "bar" })
+    ).toMatchObject({
       continue: true,
       success: false,
     });
+  });
+});
+
+// KEYS - executeUserCommand()
+describe("Controller Functions - executeUserCommand() - KEYS", () => {
+  // check valid input
+  it("KEYS", () => {
+    expect(
+      executeUserCommand({ action: executionCommands.keys })
+    ).toMatchObject({ continue: true, success: true });
+  });
+
+  // check invalid input after adding a key
+  it("KEYS", () => {
+    executeUserCommand({
+      action: executionCommands.add,
+      input1: "foo",
+      input2: "bar",
+    });
+    expect(
+      executeUserCommand({ action: executionCommands.keys })
+    ).toMatchObject({ continue: true, success: true });
   });
 });
