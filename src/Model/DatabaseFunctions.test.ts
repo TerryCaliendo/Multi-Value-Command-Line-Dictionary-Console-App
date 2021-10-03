7ac29d5251248fc8c75a8dd1fb2c89a3fb040cb0
Terry Caliendo <terry@dedicatedmanagers.com>
2021-10-02 17:07:10 -0700

    Testing: Keys

--- src/Model/DatabaseFunctions.test.ts
+++ src/Model/DatabaseFunctions.test.ts
@@ -1,8 +1,13 @@
-import { databaseAdd } from "./Database.Functions";
+import { databaseAdd, databaseKeys } from "./Database.Functions";
+import { executionCommands } from "../types/commands";
+
 //////////////////////////////
 // Database Functions
 //////////////////////////////
 
+/////////////////
+// ADD
+/////////////////
 // - databaseAdd()
 describe("Database Functions - databaseAdd()", () => {
   // check valid initial member addition
@@ -10,6 +15,7 @@ describe("Database Functions - databaseAdd()", () => {
     expect(databaseAdd("foo", "bar")).toMatchObject({
       success: true,
       continue: true,
+      command: executionCommands.add,
       message: "Added",
     });
   });
@@ -18,6 +24,26 @@ describe("Database Functions - databaseAdd()", () => {
     expect(databaseAdd("foo", "bar")).toMatchObject({
       success: false,
       continue: true,
+      command: executionCommands.add,
+    });
+  });
+});
+
+/////////////////
+// KEYS
+/////////////////
+// - databaseAdd()
+describe("Database Functions - databaseKeys()", () => {
+  // check for multiple keys
+  it("KEYS", () => {
+    databaseAdd("foo", "bar");
+    databaseAdd("fiz", "buzz");
+    databaseAdd("boo", "bam");
+    expect(databaseKeys()).toMatchObject({
+      success: true,
+      continue: true,
+      message: "0) foo\n1) fiz\n2) boo",
+      command: executionCommands.keys,
     });
   });
 });
